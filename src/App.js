bfac540dee4e7cca246a0d77c3cffd3599ea22ae
kamrul1 <kamrul01@yahoo.co.uk>
2020-05-08 02:27:21 +0100

    Start of Adding Time Travel

--- src/App.js
+++ src/App.js
@@ -1,38 +1,54 @@
 import React from "react";
 import "./styles.css";
 
+import {calculateWinner} from "./CalculateWinner";
+
 function Square(props) {
-    return (
-      <button 
-        className="square" 
-        onClick={props.onClick}
-      >
-        {props.value}
-      </button>
-    );
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
-handleClick(i) {
-  const squares = this.state.squares.slice();
-  squares[i] = 'X';
-  this.setState({squares: squares});
-}
-
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClick={()=>this.handleClick(i)}/>;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = "Next player: X";
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
